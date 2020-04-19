87ab01513f13ec6bb20524fa5eafb09bfedcf8a3
prestonwong7 <prestonwong7@gmail.com>
2020-04-18 21:10:04 -0700

    Cleaned up code for player and added animation code

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,17 +1,26 @@
 extends KinematicBody
 
-export var speed = 100
-export var acceleration = 100
-export var gravity = 200
+export var speed = 300
+export var sprint_speed = 500
+export var max_speed = 300
+export var crouch_speed = 50
+export var fallspeed = -5
+export var acceleration = 100	
+
+export var gravity = -100
 export var jump_power = 8000
-export var mouse_sensitivity = 0.3
-export var sprint_speed = 200
-export var max_speed = 100
-export var dash_distance = 1000
+export var dash_distance = 2000
 export var dash_cooldown = 0.2
 
-onready var head = $Head
-onready var camera = $Head/Camera 
+export var mouse_sensitivity = 0.3
+
+onready var rotation_helper = $Rotation_Helper
+onready var camera = $Rotation_Helper/Model/Camera 
+onready var crouchtween = get_node("CrouchTween")
+onready var raycollision = get_node("RayCollision")
+
+var direction = Vector3()
+var rotation_helper_basis
 
 var velocity = Vector3()
 var camera_x_rotation = 0
@@ -20,6 +29,20 @@ var timer_prevent_dash = 0
 
 var check_double_tap = false
 
+# To make weapon shoot
+var animation_manager
+
+var current_weapon_name = "UNARMED"
+var weapons = {"UNARMED":null, "KNIFE":null, "PISTOL":null, "RIFLE":null}
+const WEAPON_NUMBER_TO_NAME = {0:"UNARMED", 1:"KNIFE", 2:"PISTOL", 3:"RIFLE"}
+const WEAPON_NAME_TO_NUMBER = {"UNARMED":0, "KNIFE":1, "PISTOL":2, "RIFLE":3}
+var changing_weapon = false
+var changing_weapon_name = "UNARMED"
+
+var health = 100
+var UI_status_label
+
+# Dash Variables
 var double_tap_forward = false
 var double_tap_left = false
 var double_tap_right = false
@@ -38,14 +61,21 @@ var dash_left = false
 var dash_right = false
 var dash_backward = false
 
+# Double Jump Variables
 var first_jump_used = false
 var double_jump_used = false
 var check_double_jump = false
 var enable_double_jump = true
 
+var crouching = false
+
+# To slide down slopes
+var has_contact = false
+const MAX_SLOPE_ANGLE = 35
+
 func _input(event):
 	if event is InputEventMouseMotion:
-		head.rotate_y(deg2rad(-event.relative.x * mouse_sensitivity)) # rotates on x axis
+		rotation_helper.rotate_y(deg2rad(-event.relative.x * mouse_sensitivity)) # rotates on x axis
 		
 		var x_delta = event.relative.y * mouse_sensitivity
 		if x_delta + camera_x_rotation > - 90 and x_delta + camera_x_rotation < 90:
@@ -53,14 +83,18 @@ func _input(event):
 			camera_x_rotation += x_delta
 
 func _physics_process(delta):
-	
+	process_input(delta)
+	process_movement(delta)
+	process_changing_weapons(delta)
+
+func process_input(delta):
 	# Get direction player is facing
-	var head_basis = head.get_global_transform().basis # get local transform and rotation axis 
-	
-	var direction = Vector3()
+	direction = Vector3() # needed so the player moves one direction after pressing one button once
+	rotation_helper_basis = rotation_helper.get_global_transform().basis # get local transform and rotation axis 
+
 	# Move forward or backward, not both
 	if Input.is_action_pressed("move_forward"):
-		direction -= head_basis.z # forward is negative on godot
+		direction -= rotation_helper_basis.z # forward is negative on godot
 		# To not accidentally dash back and forth (mashing keys)
 		check_backward_dash = false
 		check_left_dash = false
@@ -71,7 +105,7 @@ func _physics_process(delta):
 			dash_forward = true
 			double_tap_forward = false
 	elif Input.is_action_pressed("move_backward"):
-		direction += head_basis.z
+		direction += rotation_helper_basis.z
 		check_forward_dash = false
 		check_left_dash = false
 		check_right_dash = false
@@ -83,7 +117,7 @@ func _physics_process(delta):
 		
 	# Move left or right, not both
 	if Input.is_action_pressed("move_left"):
-		direction -= head_basis.x
+		direction -= rotation_helper_basis.x
 		check_forward_dash = false
 		check_backward_dash = false
 		check_right_dash = false
@@ -93,7 +127,7 @@ func _physics_process(delta):
 			dash_left = true
 			double_tap_left = false
 	elif Input.is_action_pressed("move_right"):
-		direction += head_basis.x
+		direction += rotation_helper_basis.x
 		check_forward_dash = false
 		check_backward_dash = false
 		check_left_dash = false
@@ -102,41 +136,64 @@ func _physics_process(delta):
 			prevent_dash = true
 			dash_right = true
 			double_tap_right = false
-		
+			
+	dash(delta)
+	jump_and_double_jump() # jump & double jump mechanics inside
+	sprint()
+	
+	if Input.is_action_pressed("crouch"):
+		crouching = true
+		crouchtween.interpolate_property(raycollision.shape, "length", 
+			raycollision.shape.length, 0, 0.08, Tween.TRANS_LINEAR,
+			Tween.EASE_IN_OUT)
+		crouchtween.start()
+		speed = crouch_speed
+	
+	# Fast-fall
+	if Input.is_action_pressed("crouch") and not(has_contact) and crouching == true:
+		velocity.y += gravity * fallspeed
+		speed = crouch_speed
+	
+	if Input.is_action_just_released("crouch"):
+		crouching = false
+		crouchtween.interpolate_property(raycollision.shape, "length", 
+			raycollision.shape.length, 2.5, 0.08, Tween.TRANS_LINEAR,
+			Tween.EASE_IN_OUT)
+			
+	if crouching == true:
+		rotation_helper_basis.y -= Vector3(0,5,0)
+			
+
+func process_movement(delta):
 	direction = direction.normalized() # so moving diagonally doesn't increase speed
 	
 	velocity = velocity.linear_interpolate(direction * speed, acceleration * delta) 
 	
-	velocity.y -= gravity # Gravity mechanics
+	# Slope logic to slide down at a certain angle
+	if is_on_floor():
+		has_contact = true
+		var n = $LegRaycast.get_collision_normal()
+		var floor_angle = rad2deg(acos(n.dot(Vector3(0,1,0))))
+		if floor_angle > MAX_SLOPE_ANGLE:
+			velocity.y += gravity
+			pass
+	else:
+		if !$LegRaycast.is_colliding():
+			has_contact = false
+			velocity.y += gravity # Gravity mechanics
+			
+	if (has_contact and !(is_on_floor())):
+		move_and_collide(Vector3(0,-1,0))
+	
+	# End slope logic
+	
 	velocity = move_and_slide(velocity, Vector3.UP)
-	
-	jump_and_double_jump() # jump & double jump mechanics inside
-	sprint()
-	
-	# Dash player in a direction
-	if dash_forward == true:
-		velocity += head_basis.z * dash_distance
-		dash_forward = false
-	elif dash_backward == true:
-		velocity -= head_basis.z * dash_distance
-		dash_backward = false
-	elif dash_left == true:
-		velocity += head_basis.x * dash_distance
-		dash_left = false
-	elif dash_right == true:
-		velocity -= head_basis.x * dash_distance
-		dash_right = false
-	
-	# Dashing forward mechanics
-	prevent_dash()
 
-	# Dash cooldown			
-	if check_timer_dash == true:
-		timer_prevent_dash += delta
-		if timer_prevent_dash > dash_cooldown:
-			prevent_dash = false
-			timer_prevent_dash = 0
-			check_timer_dash = false
+func process_changing_weapons(delta):
+	if changing_weapon == true:
+		
+		var weapon_unequipped = false
+		var current_weapon = weapons[current_weapon_name]
 
 func jump_and_double_jump():
 	if is_on_floor():
@@ -148,9 +205,10 @@ func jump_and_double_jump():
 		and enable_double_jump == false and first_jump_used == false):
 			check_double_jump = true # used in _process(delta)
 
-	if (Input.is_action_just_pressed("jump") and is_on_floor()):
+	if (Input.is_action_just_pressed("jump") and has_contact):
 		velocity.y -= jump_power
 		first_jump_used = true
+		has_contact = false
 		print("jump")
 		
 	if first_jump_used == true:
@@ -174,9 +232,23 @@ func sprint():
 		speed = max_speed
 	pass
 
-func prevent_dash():
+func dash(delta): # Dash player in a direction
+	if dash_forward == true:
+		print ("dashed forward")
+		velocity += rotation_helper_basis.z * dash_distance
+		dash_forward = false
+	elif dash_backward == true:
+		velocity -= rotation_helper_basis.z * dash_distance
+		dash_backward = false
+	elif dash_left == true:
+		velocity += rotation_helper_basis.x * dash_distance
+		dash_left = false
+	elif dash_right == true:
+		velocity -= rotation_helper_basis.x * dash_distance
+		dash_right = false
+
 	if prevent_dash == false: # Prevent_dash = cooldown checker
-		if (Input.is_action_just_released("move_forward") and is_on_floor()):
+		if (Input.is_action_just_released("move_forward") and has_contact):
 			check_double_tap = true
 			check_forward_dash = true
 			check_left_dash = false
@@ -200,10 +272,43 @@ func prevent_dash():
 			check_forward_dash = false
 			check_left_dash = false
 			check_backward_dash = false
-
+			
+	# Dash cooldown			
+	if check_timer_dash == true:
+		timer_prevent_dash += delta
+		if timer_prevent_dash > dash_cooldown:
+			prevent_dash = false
+			timer_prevent_dash = 0
+			check_timer_dash = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# Player weapon stuff below
+	
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+	camera = $Rotation_Helper/Model/Camera
+	rotation_helper = $Rotation_Helper
+	
+	weapons["KNIFE"] = $Rotation_Helper/Gun_Fire_Points/Knife_Point
+	weapons["PISTOL"] = $Rotation_Helper/Gun_Fire_Points/Pistol_Point
+	weapons["RIFLE"] = $Rotation_Helper/Gun_Fire_Points/Rifle_Point
+	
+	var gun_aim_point_pos = $Rotation_Helper/Gun_Aim_Point.global_transform.origin
+	
+	for weapon in weapons:
+		var weapon_node = weapons[weapon]
+		if weapon_node != null:
+			weapon_node.player_node = self
+			weapon_node.look_at(gun_aim_point_pos, Vector3(0, 1, 0))
+			weapon_node.rotate_object_local(Vector3(0, 1, 0), deg2rad((180)))
+			
+	current_weapon_name = "UNARMED"
+	changing_weapon_name = "UNARMED"
+	
+	UI_status_label = $HUD/Panel/Gun_label
+	#flashlight = $Rotation_Helper/Flashlight
+	
+	
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
